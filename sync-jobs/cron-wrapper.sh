554f0961a27df2c5ecf99178a0b9dcdc6561dedb
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-12-12 11:28:13 +0100

    sync-jobs: add latest symlink

--- sync-jobs/cron-wrapper.sh
+++ sync-jobs/cron-wrapper.sh
@@ -6,3 +6,4 @@ LOG=/local00/sync/sync-jobs_$(date +'%Y-%m-%d_%H:%M:%S').txt
 # remove old log files
 echo -e "\nremoving old log files:" >> $LOG
 find /local00/sync -mtime +2  -print -delete &>> $LOG
+ln -sf $LOG /local00/sync/latest.txt
