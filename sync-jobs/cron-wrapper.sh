08307745fb91c8b00637b3f544164a2ac6400188
Rémi Palancher <remi-externe.palancher@edf.fr>
2017-12-12 11:43:44 +0100

    sync-jobs: timestamp log files in header
    This is particularly useful when using latest symlink.

--- sync-jobs/cron-wrapper.sh
+++ sync-jobs/cron-wrapper.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 LOG=/local00/sync/sync-jobs_$(date +'%Y-%m-%d_%H:%M:%S').txt
+echo "Run: $(date)" > $LOG
 # run utility
-/usr/local/sbin/scibian-sync-jobs &> $LOG
+/usr/local/sbin/scibian-sync-jobs &>> $LOG
 # remove old log files
 echo -e "\nremoving old log files:" >> $LOG
 find /local00/sync -mtime +2  -print -delete &>> $LOG
